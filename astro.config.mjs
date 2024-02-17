438309de902b8074a144f643a1f15a6ff544c56e
Napolux <napolux@gmail.com>
2024-02-17 15:55:01 +0100

    chore(setup): cloudflare SSR setup

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,12 @@
 import { defineConfig } from 'astro/config';
 import react from "@astrojs/react";
-
+import cloudflare from '@astrojs/cloudflare';
 import tailwind from "@astrojs/tailwind";
+
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [react(), tailwind()]
+  integrations: [react(), tailwind()],
+  output: "server",
+  adapter: cloudflare()
 });
